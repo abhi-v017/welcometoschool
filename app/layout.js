f94e0e9d23991e9fc42de04a5ae1f2e4df80977c
abhishek verma <abhi.av017@gmail.com>
2024-05-23 22:22:09 +0530

    basic layout completed

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,12 @@
-import { Inter } from "next/font/google";
+
+import { Schoolbell } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const schoolbell_init = Schoolbell({
+  subsets:['latin'],
+  weight:'400',
+  variable:"--font-schoolbell"
+})
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={schoolbell_init.variable}>{children}</body>
     </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -2,111 +2,118 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="bg-zinc-300">
+      <div className="mx-8 px-10">
+        <h1 className="text-center pt-12 font-bold text-3xl schoolbell">Welcome to the school</h1>
+        <nav className=" p-6">
+          <ul className="flex schoolbell font-bold text-lg justify-between  items-center">
+            <li>About</li>
+            <li>Methodology</li>
+            <li>Admission</li>
+            <li>Facilities</li>
+            <li>Know us</li>
+            <input className="w-1/5 border-2 border-black rounded-sm " />
+          </ul>
+        </nav>
+        <div className="bg-violet-400 border-2 border-black text-center h-screen my-3">
+          <div>
+            <h1 className="schoolbell text-fuchsia-500 bordertext font-bold fontxbold">REGISTRATION <span className="text-yellow-300">OPEN</span></h1>
+            <h1 className="schoolbell fontxbold text-white bordertext font-bold">UNTILL JULY 30</h1>
+            <p>vkfvsfj kvbsbvds vbsjbvxbs vhdsbvkd </p>
+            <p>vkfvsfj kvbsbvds vbsjbvxbs vhdsbvkd d sdhfshc</p>
+            <p>vkfvsfj kvbsbvds vbsjbvxbs vhdsbvkd d sdhfshc dsc sdoc dsc dsc</p>
+          </div>
         </div>
       </div>
+      <div className="mx-8 px-10 h-screen">
+        <div className="mx-6">
+          <div className="pt-6 schoolbell">
+            <h1 className="text-fuchsia-500 bordertext font-bold fontxbold">Know our</h1>
+            <h1 className="text-yellow-300 bordertext font-bold fontxbold ">Methodology</h1>
+          </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+          <p>vkfvsfj kvbsbvds vbsjbvxbs vhdsbvkd </p>
+          <p>vkfvsfj kvbsbvds vbsjbvxbs vhdsbvkd d sdhfshc</p>
+          <p>vkfvsfj kvbsbvds vbsjbvxbs vhdsbvkd d sdhfshc dsc sdoc </p>
+          <p>vkfvsfj kvbsbvds vbsjbvxbs vhdsbvkd d sdhfshc</p>
+        </div>
+        <button className="bg-black mx-6 px-8 text-2xl font-bold text-white p-2  schoolbell">Read More</button>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="h-screen  m-auto  flex justify-center items-center">
+        <div className="bg-violet-400 w-2/5 h-3/4 border-4 border-black">
+          <div className="text-center m-5 p-5 schoolbell">
+            <h2 className="text-yellow-300 bordertext font-bold fontxbold"><span className="text-fuchsia-500 bordertext font-bold fontxbold">sign up</span>  for our</h2>
+            <h2 className="text-yellow-300 bordertext font-bold fontxbold">parents workshop</h2>
+          </div>
+          <div className="flex justify-center flex-col items-center gap-5 m-5 p-5">
+            <input className="border-2 border-black bg-zinc-300" />
+            <input className="border-2 border-black bg-zinc-300" />
+            <button className="bg-black text-white rounded-md p-2 my-5 schoolbell">sign up</button>
+          </div>
+        </div>
+      </div>
+      <div className="h-screen flex items-center justify-center">
+        <div className="bg-teal-400 w-full mx-8 ">
+          <h2 className="mx-6 text-yellow-300 bordertext font-bold fontxbold schoolbell">2nd</h2>
+          <div className="mx-6 font-bold text-xl schoolbell">
+            <h2 className="text-fuchsia-500 bordertext font-bold fontxbold ">REGISTRATION</h2>
+            <h2 className="text-fuchsia-500 bordertext font-bold fontxbold my-7">PERIOD</h2>
+          </div>
+          <div className="flex flex-col mx-6 font-bold text-xl">
+            <span >ALL GRADES OF EDUCATION</span>
+            <span >2021-2022 CYCLE</span>
+          </div>
+        </div>
+      </div>
+      <div className="h-screen text-center schoolbell text-teal-400 bordertext font-bold fontxbold ">
+        Take advantage
+      </div>
+      <div className="h-screen flex justify-center items-center">
+        <div className="bg-yellow-300 border-2 border-black w-full mx-8" >
+          {/* <div className="text-center">
+            <h2 className="schoolbell text-fuchsia-500 bordertext font-bold fontxbold ">sign up</h2>
+            <span className="schoolbell text-teal-400 bordertext font-bold fontxbold ">TAKE ADVANTAGE OF</span>
+            <span className="schoolbell text-teal-400 bordertext font-bold fontxbold">OUR PROMOTIONS</span>
+            <p>nkkddd d sd ld dd dlda ddaldshda ksv kshfksdh </p>
+            <p>nkkddd d sd ld dd dlda ddaldshda ksv kshfksdh dksvisdis</p>
+            <p>nkkddd d sd ld dd dlda ddaldshda ksv kshfksdh dksv</p>
+          </div>
+          <div className="flex justify-center flex-col items-center gap-5 m-5 p-5">
+            <input className="border-2 border-black bg-zinc-300" />
+            <input className="border-2 border-black bg-zinc-300" />
+            <button className="bg-black text-white rounded-md p-2 my-5 schoolbell font-bold text-xl">sign up</button>
+          </div> */}
+          <div className="flex justify-center flex-col gap-2 schoolbell">
+            <span className=" text-fuchsia-500 bordertext font-bold fontxbold">sign up</span>
+            <span className=" text-teal-400 bordertext font-bold fontxbold">TAKE ADVANTAGE OF</span>
+            <span className="schoolbell text-teal-400 bordertext font-bold fontxbold">OUR PROMOTIONS</span>
+            <p>nkkddd d sd ld dd dlda ddaldshda ksv kshfksdh </p>
+            <p>nkkddd d sd ld dd dlda ddaldshda ksv kshfksdh dksvisdis</p>
+            <p>nkkddd d sd ld dd dlda ddaldshda ksv kshfksdh dksv</p>
+            <input className="bg-zinc-300 text-black" placeholder="email" />
+            <button className="bg-teal-400 text-black font-bold text-2xl">sign up</button>
+          </div>
+        </div>
+      </div>
+      <div className="h-1/2 flex justify-center items-center">
+        <div className="bg-black text-white w-full m-8 ">
+          <h2 className="text-center mt-8 schoolbell font-bold fontxbold">welcome to the school</h2>
+          <div className="flex justify-between">
+            <ul className="gap-2 m-6 p-6 schoolbell font-bold text-2xl">
+              <li className="p-4">About</li>
+              <li className="p-4">Methodology</li>
+              <li className="p-4">facilities</li>
+              <li className="p-4">Admission</li>
+              <li className="p-4">know us</li>
+            </ul>
+            <div className="flex justify-center flex-col gap-2 schoolbell">
+              <span className=" text-yellow-300 bordertext font-bold fontxbold">sign up</span>
+              <span className=" text-yellow-300 bordertext font-bold fontxbold">for our newsletter</span>
+              <input className="bg-zinc-300 text-black" placeholder="email" />
+              <button className="bg-teal-400 text-black font-bold text-2xl">sign up</button>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
